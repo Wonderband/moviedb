efe733ecbb3d3a121bc7424a2a39cddd28242090
Marta <maardiakiv@gmail.com>
2022-11-29 20:46:18 +0100

    initial modals

---
+++ src/js/modal.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_KEY = '0fd1ddf45233c721325ad47f082cd332';
+const MOVIE_URL =
+  'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY;
+
+function fetchMovie() {}
