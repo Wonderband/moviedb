a066e4f230180e031ba7726e14181f40c9efeed8
Marta <maardiakiv@gmail.com>
2022-11-30 19:37:24 +0100

    add insert content in modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,72 @@
-import axios from 'axios';
+import requestWithKey from './requestWithKey';
 import createModal from '../templates/modal.hbs';
+import notFoundImg from '../jpg/not-found-img.png';
 
-const modalNode = document.querySelector('.backdrop');
+const modalNode = document.querySelector('.modal');
+const testButton = document.querySelector('.test-button');
+const moviesGallery = document.querySelector('.gallery');
+const movieNode = document.querySelector('.movie');
 
-const API_KEY = '0fd1ddf45233c721325ad47f082cd332';
+const addToWathched = document.createElement('button');
+addToWathched.textContent = 'ADD TO WATCHED';
+const addToQueue = document.createElement('button');
+addToQueue.textContent = 'ADD TO QUEUE';
+
 const MOVIE_URL = 'https://api.themoviedb.org/3/movie/';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+testButton.addEventListener('click', onOpenModal);
+moviesGallery.addEventListener(
+  'click',
+  event => {
+    event.preventDefault();
+    // console.dir(event.target);
+    if (event.target.className == 'movie') console.dir(event.target);
+  },
+  true
+);
 
 function onOpenModal(event) {
+  event.preventDefault();
+  // if (event.target.tagName == 'DIV')
   modalNode.classList.remove('.visually-hidden');
-  const movieID = event.target.dataset.id;
-  getMovie(MOVIE_URL + movieID);
+  // const movieID = event.target.dataset.id;
+  const movieID = 436270;
+  getMovie(MOVIE_URL + movieID)
+    .then(data => {
+      const movieObj = { ...data };
+      movieObj.poster = data.poster_path
+        ? IMG_URL + data.poster_path
+        : notFoundImg;
+      movieObj.genres = data.genres.map(el => el.name).join(', ');
+      movieObj.about =
+        data.overview ?? 'Sorry, there is no information about this movie';
+      const markup = createModal(movieObj);
+      insertIntoModal(markup);
+    })
+    .catch(err => console.log(err));
 }
+
+function onCloseModal(event) {
+  //removeEventListener
+  //clear modal content
+}
+
+function insertIntoModal(movieData) {
+  modalNode.insertAdjacentHTML('beforeend', movieData);
+  modalNode.append(addToWathched, addToQueue);
+}
+
+// document.addEventListener('keydown', function(event){
+// 	if(event.key === "Escape"){
+// 		//do something
+// 	}
+// });
 
 async function getMovie(request) {
   try {
-    const responce = await axios.get(request, { params: { api_key: API_KEY } });
-    return responce;
+    const responce = await requestWithKey(request);
+    return responce.data;
   } catch (err) {
     throw new Error(err);
   }
