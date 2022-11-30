8432c71fe4d9b6eefe11c217803661f300e4e849
Marta <maardiakiv@gmail.com>
2022-11-30 14:50:56 +0100

    rewrite with handlebars

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
+import createModal from '../templates/modal.hbs';
+
+const modalNode = document.querySelector('.backdrop');
 
 const API_KEY = '0fd1ddf45233c721325ad47f082cd332';
-const MOVIE_URL =
-  'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY;
+const MOVIE_URL = 'https://api.themoviedb.org/3/movie/';
 
-function fetchMovie() {}
+function onOpenModal(event) {
+  modalNode.classList.remove('.visually-hidden');
+  const movieID = event.target.dataset.id;
+  getMovie(MOVIE_URL + movieID);
+}
+
+async function getMovie(request) {
+  try {
+    const responce = await axios.get(request, { params: { api_key: API_KEY } });
+    return responce;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
